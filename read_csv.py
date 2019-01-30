987fdf75fc597bb98e10e7d5cbf20555705892b8
Yu Liang <yu.liang@cern.ch>
2019-01-30 09:26:09 -0500

    add open csv file

---
+++ read_csv.py
@@ -0,0 +1,16 @@
+import csv
+import Tkinter
+import tkFileDialog
+import os
+
+root = Tkinter.Tk()
+root.withdraw()
+CSV_filetypes = [('all files', '.*'), ('csv files', '.csv')]
+CSV_file_path = tkFileDialog.askopenfilename(parent=root, initialdir=os.getcwd(),
+                                             title="Please select a file:", filetypes=CSV_filetypes)
+root.destroy()
+
+csvFile = open(CSV_file_path, "r")
+reader = csv.reader(csvFile)
+for csv_line in reader:
+    print csv_line
